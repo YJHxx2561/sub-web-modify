13d56c1d7c05ddd2f5561b746852cd3efcbf2962
YJHxx2561 <167593692+YJHxx2561@users.noreply.github.com>
2025-10-13 12:35:03 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -28,7 +28,7 @@
               </el-form-item>
               <el-form-item label="后端地址:">
                 <el-select v-model="form.customBackend" allow-create filterable @change="selectChanged"
-                  placeholder="可输入自己的后端" style="width: 100%">
+                  placeholder="https://jpmsubapi.yjhup.com" style="width: 100%">
                   <el-option v-for="(v, k) in options.customBackend" :key="k" :label="k" :value="v"></el-option>
                 </el-select>
               </el-form-item>
@@ -339,7 +339,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "自用专属后端": "http://subapi2.yjhup.com:25500",
+          "自用专属后端": "https://jpmsubapi.yjhup.com",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -359,6 +359,15 @@ export default {
           { value: "https://api.wcc.best" },
         ],
         remoteConfig: [
+                    {
+            label: "自建规则",
+            options: [
+              {
+                label: "自建规则",
+                value: "https://raw.githubusercontent.com/YJHxx2561/rules-for-clash/refs/heads/main/yjhclash-blackmatrix7.ini"
+              }
+            ]
+          },
           {
             label: "CM规则",
             options: [
@@ -1313,3 +1322,4 @@ export default {
   }
 };
 </script>
+
